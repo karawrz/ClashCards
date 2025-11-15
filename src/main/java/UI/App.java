f504204a348a57dd409ec7bd734867f6de26b196
Bianca Carrer <gamergirllua@gmail.com>
2025-11-15 16:02:31 -0300

    TUDO RESOLVIDO

--- src/main/java/UI/App.java
+++ src/main/java/UI/App.java
@@ -2,62 +2,37 @@ package UI;
 
 import CORE.Carta;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 import static javafx.application.Application.launch;
 
 public class App extends Application {
 
-    public void start(Stage stage) {
-
-        //tamanho da tela
-        int scrWidth = 1200;
-        int scrHeight = 650;
-        stage.setResizable(false);
-
-        //
-        BorderPane root = new BorderPane();
-        root.getStyleClass().add("root-pane");
-        HBox centro = new HBox(50);
-        VBox esquerdaVb = new VBox(10);
-        VBox direitaVb = new VBox(10);
-        Label lblTitulo = new Label("CLASHCARDS");
-        Scene cena = new Scene(root, scrWidth, scrHeight);
-
-        ChoiceBox<String> cbRaridade = new ChoiceBox<>();
-        for(Carta.enumRaridade raridade : Carta.enumRaridade.values()){
-            cbRaridade.getItems().addAll(raridade.toString());
-        }
-        cbRaridade.setValue("Comum");
-        cbRaridade.getStyleClass().add("choice-box");
-
-        Label lblRaridade = new Label("RARIDADE:");
-        esquerdaVb.getChildren().addAll(lblRaridade, cbRaridade);
-
-        esquerdaVb.setPadding(new javafx.geometry.Insets(40, 40, 40, 200));
-        esquerdaVb.setAlignment(Pos.TOP_LEFT);
-
-        centro.setAlignment(Pos.CENTER);
-        root.setTop(lblTitulo);
-        root.setLeft(esquerdaVb);
-        root.setRight(direitaVb);
-        BorderPane.setAlignment(lblTitulo, Pos.CENTER);
-
-
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("scene.fxml"));
+        Parent root = loader.load();
         stage.setTitle("ClashCards");
-
-        lblTitulo.getStyleClass().add("titulo");
-        //arquivo do css
-        cena.getStylesheets().add(getClass().getResource("/Application.css").toExternalForm());
-        stage.setScene(cena);
+        stage.setResizable(false);
+        stage.setScene(new Scene(root));
         stage.show();
-        }
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
+
