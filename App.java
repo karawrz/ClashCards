5ea54810e24e6ea57c99780db79e538c0ec7fea5
Bianca Carrer <Gamergirllua@gmail.com>
2025-11-15 15:23:29 -0300

    scene builder

---
+++ App.java
@@ -0,0 +1,37 @@
+package UI;
+
+import CORE.Carta;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.application.Application.launch;
+
+public class App extends Application {
+
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("scene.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("ClashCards");
+        stage.setResizable(false);
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
