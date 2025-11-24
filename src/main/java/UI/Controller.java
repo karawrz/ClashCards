c119e980382092e80d4ec6ade43200bb8e1d6057
Bianca Carrer <gamergirllua@gmail.com>
2025-11-23 21:17:52 -0300

    blablabla

--- src/main/java/UI/Controller.java
+++ src/main/java/UI/Controller.java
@@ -1,7 +1,14 @@
 package UI;
 
-import CORE.Deck;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class Controller {
+    Label custoMedioContainer;
 
+    @FXML
+    protected void initialize(){
+        custoMedioContainer.setText("abcd");
+
+    }
 }
